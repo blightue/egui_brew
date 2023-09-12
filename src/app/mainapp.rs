09cf56ebd6b39ee9c91014df2a4a4bff503639f6
suisuishou <blightue@gmail.com>
2023-09-12 22:54:57 +0800

    impl sample app

---
+++ src/app/mainapp.rs
@@ -0,0 +1,30 @@
+use eframe::egui;
+
+pub struct MainApp{
+    name: String,
+    age: u32,
+}
+
+impl MainApp {
+    pub fn new() -> Self {
+        Self { name: "David".to_owned(), age: 32 }
+    }
+}
+
+impl eframe::App for MainApp {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Brew egui");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+use brew_egui::app::mainapp::MainApp;
+
+fn main() -> Result<(), eframe::Error>{
+
+    let options = eframe::NativeOptions{
+        initial_window_size: Some(egui::vec2(400.0, 400.0)),
+        ..Default::default()
+    };
+
+    eframe::run_native("Brew_EGUI", options, Box::new(|_cc| Box::new(MainApp::new())))
 }
