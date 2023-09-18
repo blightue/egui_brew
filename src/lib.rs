781cb7514aa5e5909b3b21730d48d32bf7fdc206
suisuishou <blightue@gmail.com>
2023-09-18 22:48:38 +0800

    add app & brew_cli mods

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod app;
+pub mod brew_cli;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use eframe::egui;
 use brew_egui::app::mainapp::MainApp;
+use brew_egui::brew_cli::commands::Commands;
+use tokio;
 
 fn main() -> Result<(), eframe::Error>{
+    let result = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(Commands::excute("list")).expect("msg");
+    println!("result: {}", result);
 
     let options = eframe::NativeOptions{
         initial_window_size: Some(egui::vec2(400.0, 400.0)),
